1a5bbe7984d9a333bc17b7a476ff1d8f67832043
Thejeeswaran Ra <136101835+rat-2005@users.noreply.github.com>
2026-07-18 13:11:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (31.01%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,30 +1,13 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        #finding the length of the smallest
-        l=9999
-        for x in range(0,len(strs)):
-            if len(strs[x])<l:
-                l=len(strs[x])
-
-        start=0
-        dup=0
-        com=strs[0]
-        if len(strs)==1:
-            return strs[0]
+        #sort it
+        strs.sort()
         final=""
-        flag=False
-        while(dup<l):
-            for x in strs[1:]:
-                if x[0:dup+1]==com[0:dup+1]:
-                    flag=True
-                else:
-                    flag=False
-                    break
-            if flag==True:
-                final=com[0:dup+1]
+        for i in range(min(len(strs[0]),len(strs[-1]))):
+            if strs[0][0:i+1]==strs[-1][0:i+1]:
+                final=strs[0][0:i+1]
+            else:
+                break
+        return final
 
-            dup=dup+1   
-        return final     
-
-
-        
+ 
