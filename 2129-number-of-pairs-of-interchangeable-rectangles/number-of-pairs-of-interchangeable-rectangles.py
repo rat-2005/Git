1398df13c5fcdcbc85c79a92c424196e0e2a8d42
Thejeeswaran Ra <136101835+rat-2005@users.noreply.github.com>
2026-07-21 15:34:10 +0530

    Time: 108 ms (34.52%) | Memory: 56.4 MB (100.00%) - LeetSync

--- 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.py
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.py
@@ -5,7 +5,7 @@ class Solution:
         #     for y in range(x+1,len(rectangles)):
         #         if rectangles[x][0]/rectangles[x][1]==rectangles[y][0]/rectangles[y][1]:
         #             num=num+1
-        # return num
+        # return num //bruteforce
 
         for x in range(0,len(rectangles)):
             rectangles[x]=rectangles[x][0]/rectangles[x][1]
@@ -19,10 +19,10 @@ class Solution:
             else:
                 hash[x]+=1
 
-        su=0
+        sukkor=0
         for x in hash:
-            su=su+(hash[x]-1)*(hash[x])//2
-        return su
+            sukkor=sukkor+(hash[x]-1)*(hash[x])//2
+        return sukkor
 
 
         
