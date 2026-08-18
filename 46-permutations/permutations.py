300a6d2648d3c65a0979d0620a0bea7910a64156
Thejeeswaran Ra <136101835+rat-2005@users.noreply.github.com>
2026-08-18 12:33:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (66.34%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,17 +1,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         final=[]
-        def recur(filled):
-            if len(filled)==len(nums):
-                final.append(filled[:])
+        def back(out,target):
+            if len(out)==target:
+                final.append(out.copy())
             for x in nums:
-                if x not in filled:
-                    filled.append(x)
-                    recur(filled)
-                    filled.pop()
+                if x in out:
+                    continue
+                out.append(x)
+                back(out,target)
+                out.pop()
 
-        recur([])
+        back([],len(nums))
         return final
+
+
 
             
         
