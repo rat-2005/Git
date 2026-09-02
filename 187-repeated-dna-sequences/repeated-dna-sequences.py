3ccfcd6960dbd09fd0a0bb27d5d8b8c7f09cbe19
Thejeeswaran Ra <136101835+rat-2005@users.noreply.github.com>
2026-09-02 17:00:47 +0530

    Time: 685 ms (37.32%) | Memory: 33 MB (34.72%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,11 +1,15 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        l=[]
-        hash={}
-        for x in range(0,len(s)-10+1):
-            if s[x:x+10] not in hash:
-                hash[s[x:x+10]]=1
-            elif s[x:x+10] not in l:
-                l.append(s[x:x+10])
+        seen = set()
+        repeated = set()
+        
+        for i in range(len(s) - 9):
+            sequence = s[i:i+10]
+            if sequence in seen:
+                repeated.add(sequence)
+            else:
+                seen.add(sequence)
+                
+        return list(repeated)
         return l
         
